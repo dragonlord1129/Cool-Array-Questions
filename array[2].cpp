290ab1c16c92af54718172ea5f24b29958112874
Bibhav Jha <132417322+dragonlord1129@users.noreply.github.com>
2023-12-18 10:29:27 +0545

    array[2].cpp

--- array[2].cpp
+++ array[2].cpp
@@ -1,3 +1,6 @@
+/*question: Given an array of N numbers and a positive integer K. The problem is to find K numbers with the most occurrences, i.e., the top K numbers having the
+maximum frequency. If two numbers have the same frequency then the number with a larger value should be given preference. The numbers should be displayed in 
+decreasing order of their frequencies. It is assumed that the array consists of at least K numbers.*/
 #include <iostream>
 #include <vector>
 using namespace std;
