363b9711e3d8a0c948899a38ec5b1ec6ffcd4949
Bibhav Jha <132417322+dragonlord1129@users.noreply.github.com>
2024-02-01 22:13:47 +0545

    Add files via upload

---
+++ 3sum.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class three_number_sum{
+private:
+    int i, j, k;
+    int target, sum, count;
+    vector<int> arr;
+
+public:
+    void input(){
+        cout << "Target=" << endl;
+        cin >> target;
+        arr = {3, 7, 9, 8, 6, 5, 4}; // can be added manually as well
+    }
+    void Solution();
+    void display(){
+        cout << "Pairs "<< "(" << arr[i] << "," << arr[j] << ")" << endl;
+    }
+    void displayTriplet(){
+        cout << "Pairs "<< "(" << arr[i] << "," << arr[j] << "," << arr[k] << ")" << endl;
+    }
+};
+void three_number_sum::Solution(){
+    sum = 0;
+    count = 0;
+    for (int a = 0; a < arr.size() - 2; a++){
+        for (int b = a + 1; b < arr.size() - 1; b++)
+        {
+            sum = arr[a] + arr[b];
+            if (sum == target && arr[a] != arr[b]){
+                i = a;
+                j = b;
+                count++;                
+                display();
+            }
+            else{
+                sum = 0;
+                for (int c = b + 1; c < arr.size(); c++){
+                    sum = arr[a] + arr[b] + arr[c];
+                    if (sum == target && arr[a] != arr[b] && arr[b] != arr[c]){
+                        i = a;
+                        j = b;
+                        k = c;
+                        count++;                        
+                        displayTriplet();
+                    }
+                }
+            }
+        }
+    }
+    if (count == 0){
+        cout << "no pair" << endl;
+    }
+}
+int main(){
+    three_number_sum a;
+    a.input();
+    a.Solution();
+
+    return 0;
+}
