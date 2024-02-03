3263909e6e63ffcdcbbe68e25c4a95dc3a20d948
Bibhav Jha <132417322+dragonlord1129@users.noreply.github.com>
2024-02-03 13:02:38 +0545

    Update productexceptself.cpp

--- productexceptself.cpp
+++ productexceptself.cpp
@@ -1,3 +1,19 @@
+/* Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0] */
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -34,7 +50,7 @@ void Solution::display(){
 
 int main(){
     Solution s1;
-    s1.display();
+    s1.display(); //prints the current array
     s1.product();
-    s1.display();
+    s1.display(); // prints the array of product of elements except self
 }
